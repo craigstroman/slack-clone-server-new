4c38021d24b8fdd6cfe0d8b3cc90adb28f54dfa0
Craig Stroman <cstroman@gmail.com>
2025-07-11 15:34:58 -0400

    Creating routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { indexPage } from '../controllers/index.js';
+
+export const router = new Router();
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('inside here: ');
+  router.route(/^\/(?!graphql).*/).get(indexPage);
+} else {
+  router.route('/').get(indexPage);
+}
