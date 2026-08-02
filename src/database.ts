5caf49b0d829586c2c20269d8e2adc49f61616e1
Craig Stroman <cstroman@gmail.com>
2026-08-02 13:11:59 -0400

    Creating database settings file

---
+++ src/database.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+
+import { DataSource } from 'typeorm';
+
+import path from 'path';
+import dotenv from 'dotenv';
+
+import { ChatMessage } from './entities/CHATMESSAGE';
+import { ChatRoom } from './entities/CHATROOM';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [ChatMessage, ChatRoom],
+  /*
+   * Use migrations in production.
+   * synchronize is convenient only during development.
+   */
+  synchronize: process.env.NODE_ENV !== 'production',
+
+  logging: process.env.NODE_ENV === 'development',
+});
