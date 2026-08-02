8acc4007b6cb828809c1720e863772f29fa67dbe
Craig Stroman <cstroman@gmail.com>
2026-08-02 12:50:16 -0400

    Creating chat message entity

---
+++ src/entities/CHATMESSAGE.ts
@@ -0,0 +1,37 @@
+import { ObjectType, Field } from 'type-graphql';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class ChatMessage extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  roomId: string;
+
+  @Field()
+  @Column()
+  username: string;
+
+  @Field()
+  @Column()
+  text: string;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt = new Date();
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt = new Date();
+}
