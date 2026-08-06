0413427c4dfbb10a90cd5111c7961de0338ed0dc
Craig Stroman <cstroman@gmail.com>
2026-08-06 14:49:45 -0400

    Removing no longer needed code from pubSub

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -2,10 +2,6 @@ import { createPubSub } from '@graphql-yoga/subscription';
 
 import type { ChatMessage } from './entities/CHATMESSAGE';
 
-export interface iCounterPayload {
-  counterUpdated: number;
-}
-
 export const MESSAGE_SENT = 'MESSAGE_SENT' as const;
 
 export const pubSub = createPubSub<{
