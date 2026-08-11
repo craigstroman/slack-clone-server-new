3d504b95d43a290cbc4e34a59ecf4a6310fdb0bb
Craig Stroman <cstroman@gmail.com>
2026-08-11 19:50:05 -0400

    Updating types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
 
-export type MyContext = {
-  req: Request & { session: Express.Session };
+declare module 'express-session' {
+  interface SessionData {
+    userId: number;
+  }
+}
+
+export interface MyContext {
   redis: Redis;
+  req: Request;
   res: Response;
-};
+  userId?: number;
+}
