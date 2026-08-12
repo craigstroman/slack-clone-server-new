6597f31ee9a12b420b14ed46c61c5f04e75e23b0
Craig Stroman <cstroman@gmail.com>
2026-08-11 20:04:31 -0400

    Adding more types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
+import { InputType, Field, ObjectType } from 'type-graphql';
+import { User } from './entities/USER';
 
 declare module 'express-session' {
   interface SessionData {
@@ -12,4 +14,35 @@ export interface MyContext {
   req: Request;
   res: Response;
   userId?: number;
+}
+
+@InputType()
+export class UsernameRegisterInput {
+  @Field()
+  first_name: string;
+  @Field()
+  last_name: string;
+  @Field()
+  email: string;
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+export class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+export class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
